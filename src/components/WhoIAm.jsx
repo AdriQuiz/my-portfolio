f243722742e76d32530a28b24484d16529a9f21d
AQz07 <christinaquirozcorrea@gmail.com>
2025-07-21 16:37:13 -0400

    fix: name change

--- src/components/WhoIAm.jsx
+++ src/components/WhoIAm.jsx
@@ -7,7 +7,7 @@ function WhoIAm() {
         <div id='whoiam' className='pt-20 lg:pt-0 lg:flex items-center justify-around mx-10 min-h-[100vh]'>
             <div className='text-center mb-16 lg:mb-0'>
                 <h4 className='text-lg text-white mb-5 font-semibold lg:text-left'>Hola, soy Adriana Quiroz</h4>
-                <h2 className='text-5xl text-blue-400 font-bold mb-10 lg:text-left'>Front-End Developer</h2>
+                <h2 className='text-5xl text-blue-400 font-bold mb-10 lg:text-left'>Software Developer</h2>
                 <div className='flex justify-center items-center lg:justify-start space-x-4'>
                     <Button content={'Contáctame'} type={'blue'} href={'#contact'} />
                     <Button content={'Proyectos'} type={'white'} href={'#projects'} />
